f6b53151ac5fb691101c6573ee31536fc2d46fff
JeremyCCox <93951833+JeremyCCox@users.noreply.github.com>
2024-11-05 22:19:07 -0700

    Implementing React Router, adding Home and Login page Components.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from "react-router-dom";
+import Home from "./components/Home.tsx";
+import Login from "./components/Login.tsx";
 
 function App() {
-    return(
-    <>
-    </>
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path="/" element={<Home/>}>
+                <Route path="dashboard" element={<Login />} /></Route>
+        )
+    );
+    return (
+        <>
+            <RouterProvider router={router}/>
+        </>
     )
 }
 
